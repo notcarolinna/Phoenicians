a53f19e3494304404c4eb93b2ea1b1893173836d
Carolina Nóbrega <notcarolinna@Gmail.com>
2023-05-03 21:52:50 -0300

    Update LosFenicios.cpp

--- LosFenicios.cpp
+++ LosFenicios.cpp
@@ -2,7 +2,9 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
-#include <queue>
+#include <stack>
+#include <climits>
+#include <cmath>
 
 class Reader {
 public:
@@ -13,79 +15,65 @@ public:
 	Reader(std::string arquivo) : arquivo(arquivo) {}
 	std::vector<std::vector<char>> mapa;
 	std::pair<int, int> start;
-	std::vector<std::pair<int, int>> targets;
+	std::vector<std::pair<int, int>> postos;
 	void read();
 };
 
-
 void Reader::read() {
 	std::ifstream file(arquivo);
-	std::string line;
-	std::vector<std::pair<int, int>> direcoes = { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
-
-	if (file.is_open()) {
-		getline(file, line);
-		std::stringstream(line) >> linhas;
-		std::stringstream(line.substr(line.find(' ') + 1)) >> colunas;
-
-		// Verificação para saber se o número de linhas e colunas
-		// foram obtidos corretamente
-		std::cout << "Linhas: " << linhas << "\nColunas: " << colunas << std::endl;
-
-		mapa = std::vector<std::vector<char>>(linhas, std::vector<char>(colunas));
-
-		start = std::make_pair(0, 0); // primeiro posto da sequência não obstruído
-		int firstTarget = 1; // primeiro alvo da sequência não obstruído
-		bool foundFirstTarget = false; // se já encontrou o primeiro posto da sequência
-
-		for (int i = 0; i < linhas; i++) {
-			getline(file, line);
-			std::vector<char> row;
-			for (int j = 0; j < colunas; j++) {
-				if (j < line.size()) {
-					row.push_back(line[j]);
-
-					if (line[j] >= '1' && line[j] <= '9') {
-						int num;
-						std::stringstream(line.substr(j, 1)) >> num;
-
-						if (num == firstTarget) {
-							start = std::make_pair(i, j);
-							firstTarget++;
-							foundFirstTarget = true;
-						}
-						targets.push_back(std::make_pair(i, j));
-					}
-				}
-			}
-			mapa[i] = row;
-		}
+	if (!file.is_open()) {
+		std::cerr << "Falha ao abrir o arquivo " << arquivo << std::endl;
+		return;
 	}
-	else {
-		std::cerr << "Nao foi possivel abrir o arquivo " << arquivo << std::endl;
+
+	std::string line;
+	std::getline(file, line);
+	std::stringstream ss(line);
+	ss >> linhas >> colunas;
+	std::cout << "Linhas: " << linhas << "\nColunas: " << colunas << std::endl;
+
+	mapa = std::vector<std::vector<char>>(linhas, std::vector<char>(colunas));
+	start = { 0, 0 };
+	int firstTarget = 1;
+	bool foundFirstTarget = false;
+
+	for (int i = 0; i < linhas; i++) {
+		std::getline(file, line);
+		std::vector<char> col(colunas, ' ');
+		for (int j = 0; j < colunas && j < line.size(); j++) {
+			col[j] = line[j];
+			if (line[j] >= '1' && line[j] <= '9') {
+				int num = line[j] - '0';
+				if (num == firstTarget) {
+					start = { i, j };
+					firstTarget++;
+					foundFirstTarget = true;
+				}
+				postos.emplace_back(i, j);
+			}
+		}
+		mapa[i] = col;
 	}
 }
+
 
 
 int main() {
 	Reader reader("teste.txt");
 	reader.read();
 
-	std::cout << "Coordenadas do primeiro posto da sequencia: (" << reader.start.first << ", " << reader.start.second << ")\n" << std::endl;
-
 	int total_distancia = 0;
+	int postos[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	int num_posto = 0;
+	int numero_atual = postos[num_posto];
 
-	int sequencia_desejada[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 1 };
-	int idx_sequencia = 0;
-	int numero_atual = sequencia_desejada[idx_sequencia];
-
-	while (idx_sequencia < 10) {
+	while (num_posto < 10) {
 		bool encontrado = false;
 
 		for (int i = 0; i < reader.linhas; i++) {
 			for (int j = 0; j < reader.colunas; j++) {
 				if (reader.mapa[i][j] == '0' + numero_atual) {
-					reader.targets.push_back(std::make_pair(i, j));
+					reader.postos.push_back(std::make_pair(i, j));
 					std::cout << "Posto " << numero_atual << " encontrado nas coordenadas (" << i << ", " << j << ")" << std::endl;
 					encontrado = true;
 					break;
@@ -94,8 +82,8 @@ int main() {
 		}
 
 		if (encontrado) {
-			idx_sequencia++;
-			numero_atual = sequencia_desejada[idx_sequencia];
+			num_posto++;
+			numero_atual = postos[num_posto];
 		}
 	}
 
